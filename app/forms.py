52a0ed1ddfa78ec59de9199a1ea6463cd2a5c304
Malavika Srinivasan <malavikasrinivasan@airbears2-10-142-102-182.airbears2.1918.berkeley.edu>
2016-10-19 11:33:05 -0700

    Adding LoginForm to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, PasswordField
 from flask_wtf.html5 import EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    ...    
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
 
 class TripForm(Form):
-    ...
